eeebdbfa1fef60c397da9801309fe0e7c1fbd210
Wojciech Koszela <wojtek.koszela@gmail.com>
2024-01-25 22:34:40 +0100

    Przyklad 41

---
+++ zjazd-4/src/main/java/pl/wbs/programowaniejava/wkoszela/zjazd4/wyjatki/przyklad41/Main.java
@@ -0,0 +1,25 @@
+package pl.wbs.programowaniejava.wkoszela.zjazd4.wyjatki.przyklad41;
+
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stream
+                .of(
+                        new Person("Andrzej", 1984),
+                        new Person("Andrzej", 2020),
+                        new Person("       ", 1984),
+                        new Person("       ", 2020))
+                .forEach(Main::validate);
+    }
+
+    public static void validate(Person person) {
+        try {
+            Validator.validateName(person);
+            Validator.validateAdult(person);
+        } catch (ValidationException ex) {
+            System.out.println("Validation failed: " + ex.getMessage());
+        }
+    }
+}
---
+++ zjazd-4/src/main/java/pl/wbs/programowaniejava/wkoszela/zjazd4/wyjatki/przyklad41/ValidationException.java
@@ -0,0 +1,8 @@
+package pl.wbs.programowaniejava.wkoszela.zjazd4.wyjatki.przyklad41;
+
+public class ValidationException extends Exception {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
