99ee89d1361c97eef8aca9b91b3a9bb160144600
Wojciech Koszela <wojtek.koszela@gmail.com>
2024-01-26 00:45:27 +0100

    Maven + Nexus | Przyklad 48

---
+++ zjazd-4/src/main/java/pl/wbs/programowaniejava/wkoszela/zjazd4/jar/przyklad48/LineCounter.java
@@ -0,0 +1,13 @@
+package pl.wbs.programowaniejava.wkoszela.zjazd4.jar.przyklad48;
+
+public class LineCounter {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Specify filename");
+            return;
+        }
+
+        FileUtils.countLines(args[0]).ifPresent(count -> System.out.printf("Lines: %d%n", count));
+    }
+}
