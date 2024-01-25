907655829b6a6d8c27ddad2486ee569619092fd3
Wojciech Koszela <wojtek.koszela@gmail.com>
2024-01-25 16:50:16 +0100

    NPE Cat

--- zjazd-4/src/main/java/pl/wbs/programowaniejava/wkoszela/zjazd4/PrzykladOptional.java
+++ zjazd-4/src/main/java/pl/wbs/programowaniejava/wkoszela/zjazd4/PrzykladOptional.java
@@ -1,6 +1,27 @@
 package pl.wbs.programowaniejava.wkoszela.zjazd4;
 
-import java.util.Scanner;
+
+import java.util.Optional;
 
 public class PrzykladOptional {
+
+    public static void main(String[] args) {
+        String name = "Filemon";
+        Cat filemon = findCatByName(name);
+
+        if(filemon != null){
+            filemon.getName();
+            System.out.println();
+        }
+        else {
+            System.out.println("Nie znaleziono Kota");
+        }
+
+    }
+
+
+    private static Cat findCatByName(String name){
+        Cat cat = new Cat(name);
+        return null;
+    }
 }
