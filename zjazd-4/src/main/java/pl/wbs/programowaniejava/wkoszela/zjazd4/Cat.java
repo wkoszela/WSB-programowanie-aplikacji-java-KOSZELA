fa36b8f205cc12959a5874a8beeb40bf658fec48
Wojciech Koszela <wojtek.koszela@gmail.com>
2024-01-25 16:50:16 +0100

    NPE Cat

---
+++ zjazd-4/src/main/java/pl/wbs/programowaniejava/wkoszela/zjazd4/Cat.java
@@ -0,0 +1,19 @@
+package pl.wbs.programowaniejava.wkoszela.zjazd4;
+
+public class Cat {
+
+    private String name;
+
+    public Cat(String name) {
+
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- zjazd-4/src/main/java/pl/wbs/programowaniejava/wkoszela/zjazd4/PrzykladOptional.java
+++ zjazd-4/src/main/java/pl/wbs/programowaniejava/wkoszela/zjazd4/PrzykladOptional.java
@@ -1,6 +1,25 @@
 package pl.wbs.programowaniejava.wkoszela.zjazd4;
 
-import java.util.Scanner;
 
 public class PrzykladOptional {
+
+    public static void main(String[] args) {
+        String name = "Filemon";
+        Cat filemon = findCatByName(name);
+
+        if(filemon.getName() != null){
+            filemon.getName();
+            System.out.println();
+        }
+        else {
+            System.out.printf("Nie znaleziono Kota");
+        }
+
+    }
+
+
+    private static Cat findCatByName(String name){
+        Cat cat = new Cat(name);
+        return null;
+    }
 }
